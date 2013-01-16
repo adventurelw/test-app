0b7bef15bd6517b51c84f818eba44a043648b009
Lavin <adventurelw@gmail.com>
2013-01-17 00:17:18 +0800

    updates

--- config/application.rb
+++ config/application.rb
@@ -64,5 +64,9 @@ module TestApp
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.generators do |g|
+      g.fixture_replacement :factory_girl, :dir => 'spec/factories'
+    end
   end
 end
